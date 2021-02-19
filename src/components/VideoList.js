f5bcfa94c6d9d055ee9c3a205f66981151b7cdd1
Hank Cao <hankcao123@gmail.com>
2021-02-18 16:15:02 -0800

    Worked on VideoList

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import VideoItem from './VideoItem'
 
 const VideoList = ({videos}) => {
-
-    return (
-        <></>
-    );
+    const renderedList = videos.map(video => {
+        return (
+            <div className = "ui relaxed divided list">
+                <VideoItem video = {video}/>
+            </div>
+        )
+    })
 }
 
 export default VideoList;
